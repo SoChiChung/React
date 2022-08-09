e05338f94c764b12b4f242f59f4190f3a2d852d6
SoChichung <ddeadwings@gmail.com>
2022-08-10 00:49:16 +0800

    2022.08.10

--- demo-react/src/view/board.jsx
+++ demo-react/src/view/board.jsx
@@ -1,7 +1,19 @@
+/*
+ * @Author: SoChichung
+ * @Date: 2022-08-07 22:25:23
+ * @LastEditors: SoChichung
+ * @LastEditTime: 2022-08-10 00:27:58
+ * @Description:
+ *
+ * Copyright (c) 2022 by SoChichung ddeadwings@gmail.com, All Rights Reserved.
+ */
 import React from "react";
 import Square from "./square";
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   returnSquare(i) {
     return <Square value={i}></Square>;
   }
